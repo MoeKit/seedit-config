62e9df824339156e2593bf153a47b756fb5f6ee2
airyland <airyland@qq.com>
2014-06-14 13:50:50 +0800

    add huodong API dectect

--- index.js
+++ index.js
@@ -1,31 +1,80 @@
-var commonAPI;
-var host = window.location.host;
-// default setting
-if (/seedit.com/.test(host) || /localhost/.test(host) || /seedit.cn/.test(host) || /^\d+.\d+.\d+.\d+/.test(host)) {
-    commonAPI = 'http://common.seedit.com';
-} else {
-    var hostArr = host.split('.');
-    if(hostArr.length > 2) {
-        hostArr.splice(0, 1);
-    }
-    commonAPI = 'http://common.' + hostArr.join('.');
-}
+var hostname = window.location.hostname;
+var commonAPI = getCommonAPI(hostname);
+var huodongAPI = getHuodongAPI(hostname);
 
 var __seeditConfig = window.__seeditConfig = window.__seeditConfig || {
-    commonAPI: commonAPI // common API
+    commonAPI: commonAPI, // common API
+    huodongAPI: huodongAPI
 };
 
 var seeditConfig = {
-    get: function(key) {
+    get: function (key) {
         if (!key) {
             return __seeditConfig;
         } else {
             return __seeditConfig[key];
         }
     },
-    set: function(key, value) {
+    set: function (key, value) {
         __seeditConfig[key] = value;
     }
 };
 
+// get scope
+// 当有 huodong 标识时， scope为 huodong
+// http://huodong.office.bzdev.net/shiyong/ =>
+// http://huodong.office.bzdev.net/restful  只在当前域可用
+// http://common.office.bzdev.net/
+
+// 其他为common
+// http://m.csf.bzdev.net
+// => http://common.csf.bzdev.net
+// => http://huodong.csf.bzdev.net
+
+
+function getScopeByUrl(url) {
+    url = url || document.location.host;
+    if (/huodong/.test(url)) {
+        return 'huodong';
+    }
+    return 'common';
+};
+
+// is localhost or cms
+function isLocal(url) {
+    return  /localhost/.test(url) || /seedit.cn/.test(url) || /^\d+.\d+.\d+.\d+/.test(url);
+}
+
+// 获取huodong API
+function getHuodongAPI(url) {
+    if (isLocal(url)) {
+        return 'http://huodong.seedit.com/restful';
+    }
+    var scope = getScopeByUrl(url);
+    // 当前不在huodong域，拼凑子域名
+    if (scope === 'common') {
+        return ['http://huodong'].concat(url.split('.').slice(1)).join('.') + '/restful';
+    } else {
+        return url + '/restful';
+    }
+};
+
+
+// 获取common API
+function getCommonAPI(url) {
+    var scope = getScopeByUrl(url);
+    // default setting
+    if (/seedit.com/.test(url) || isLocal(url)) {
+        return 'http://common.seedit.com';
+    } else {
+        var hostArr = url.split('.');
+        if (hostArr.length > 2) {
+            hostArr.splice(0, 1);
+        }
+        return 'http://common.' + hostArr.join('.');
+    }
+};
+
 module.exports = seeditConfig;
+module.exports.getCommonAPI = getCommonAPI;
+module.exports.getHuodongAPI = getHuodongAPI;
--- tests/seedit-config-spec.js
+++ tests/seedit-config-spec.js
@@ -1,40 +1,73 @@
 var expect = require('expect.js');
 var seeditConfig = require('../index');
+var getCommonAPI = seeditConfig.getCommonAPI;
+var getHuodongAPI = seeditConfig.getHuodongAPI;
 
-describe('read config', function() {
-  
-  it('get method exists', function(){
-    expect(seeditConfig.get).to.be.a('function');
-  });
- 
-  it('window.__seeditConfig exists',function(){
-    expect(window.__seeditConfig).to.be.an('object');
-  });
+describe('read config', function () {
 
-  it('get all config', function() {
-    expect(seeditConfig.get().commonAPI).to.be('http://common.seedit.com');
-  });
-    
-  it('get single config',function(){
-    expect(seeditConfig.get('commonAPI')).to.be('http://common.seedit.com');
-  });
+    it('get method exists', function () {
+        expect(seeditConfig.get).to.be.a('function');
+    });
+
+    it('window.__seeditConfig exists', function () {
+        expect(window.__seeditConfig).to.be.an('object');
+    });
+
+    it('get all config', function () {
+        expect(seeditConfig.get().commonAPI).to.be('http://common.seedit.com');
+        expect(seeditConfig.get().huodongAPI).to.be('http://huodong.seedit.com/restful');
+    });
+
+    it('get single config', function () {
+        expect(seeditConfig.get('commonAPI')).to.be('http://common.seedit.com');
+    });
 
 });
 
-describe('write config', function() {
+describe('write config', function () {
 
-  it('set method exists', function(){
-    expect(seeditConfig.set).to.be.a('function');
-  });
+    it('set method exists', function () {
+        expect(seeditConfig.set).to.be.a('function');
+    });
 
-  it('get all config', function() {
-    seeditConfig.set('commonAPI','http://a.seedit.com');
-    expect(seeditConfig.get().commonAPI).to.be('http://a.seedit.com');
-  });
-    
-  it('get single config',function(){
-    seeditConfig.set('commonAPI','http://b.seedit.com');
-    expect(seeditConfig.get('commonAPI')).to.be('http://b.seedit.com');
-  });
+    it('get all config', function () {
+        seeditConfig.set('commonAPI', 'http://a.seedit.com');
+        expect(seeditConfig.get().commonAPI).to.be('http://a.seedit.com');
+    });
+
+    it('get single config', function () {
+        seeditConfig.set('commonAPI', 'http://b.seedit.com');
+        expect(seeditConfig.get('commonAPI')).to.be('http://b.seedit.com');
+    });
+
+});
+
+
+describe('detect function', function () {
+    it('少凡m站配置测试', function () {
+        expect(getCommonAPI('http://m.csf.bzdev.net')).to.be('http://common.csf.bzdev.net');
+        expect(getHuodongAPI('http://m.csf.bzdev.net')).to.be('http://huodong.csf.bzdev.net/restful');
+    });
+
+    it('活动office站配置测试', function () {
+        expect(getCommonAPI('http://huodong.office.bzdev.net')).to.be('http://common.office.bzdev.net');
+        expect(getHuodongAPI('http://huodong.office.bzdev.net')).to.be('http://huodong.office.bzdev.net/restful');
+    });
+
+    it('承林huodong站配置测试', function () {
+        expect(getCommonAPI('http://huodong.wcl.bzdev.net')).to.be('http://common.wcl.bzdev.net');
+        expect(getHuodongAPI('http://huodong.wcl.bzdev.net')).to.be('http://huodong.wcl.bzdev.net/restful');
+    });
+
+    it('活动online站配置测试', function () {
+        expect(getCommonAPI('http://huodong.online.bozhong.com')).to.be('http://common.online.bozhong.com');
+        expect(getHuodongAPI('http://huodong.online.bozhong.com')).to.be('http://huodong.online.bozhong.com/restful');
+    });
+
+    it('线上活动配置测试', function () {
+        expect(getCommonAPI('http://huodong.seedit.com')).to.be('http://common.seedit.com');
+        expect(getHuodongAPI('http://huodong.seedit.com')).to.be('http://huodong.seedit.com/restful');
+    });
+
 
 });
